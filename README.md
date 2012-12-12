ab22210915dc767ea15a7d6a8c287752b9700372
Andreas Lind Petersen <andreas@one.com>
2012-12-13 00:17:53 +0100

    Initial commit, release 0.0.1.

---
+++ README.md
@@ -0,0 +1,84 @@
+node-memoizesync
+================
+
+Yet another memoizer for synchronous functions.
+
+```javascript
+var memoizeSync = require('memoizesync');
+
+function myExpensiveComputation(arg1, arg2) {
+    // ...
+    return result;
+}
+
+var memoized = memoizeSync(myExpensiveComputation);
+```
+
+Now `memoized` works exactly like `myExpensiveComputation`, except that
+the actual computation is only performed once for each unique set of
+arguments (apart from the callback):
+
+```javascript
+var result = memoized(42, 100);
+// Got the result!
+
+var result2 = memoized(42, 100);
+// Got the same result, and much faster this time!
+```
+
+The function returned by `memoizeSync` invokes the wrapped function
+in the context it's called in itself, so `memoizeSync` even works for
+memoizing a method that has access to instance variables:
+
+```javascript
+function Foo(name) {
+    this.name = name;
+}
+
+Foo.prototype.myMethod = memoizeSync(function (arg1, arg2) {
+    console.warn("Cool, this.name works here!", this.name);
+    // ...
+    return "That was tough, but I'm done now!";
+});
+```
+
+To distinguish different invocations (whose results need to be cached
+separately) `memoizeSync` relies on a naive stringification of the
+arguments, which is looked up in an internally kept hash. If the
+function you're memoizing takes non-primitive arguments you might want
+to provide a custom `argumentsStringifier` as the second argument to
+`memoizeSync`. Otherwise all object arguments will be considered equal
+because they stringify to `[object Object]`:
+
+```javascript
+var memoized = memoizeSync(function functionToMemoize(obj) {
+    // ...
+    return Object.keys(obj).join('');
+}, function argumentStringifier(args) {
+   return args.map(function (arg) {return JSON.stringify(arg);}).join(",");
+});
+
+memoized({foo: 'bar'}); // 'foo'
+
+memoized({quux: 'baz'}); // 'quux'
+```
+
+Had the custom `argumentsStringifier` not been provided, the memoized
+function would would have returned `foo` both times.
+
+Check out <a
+href="https://github.com/papandreou/node-memoizesync/blob/master/test/memoizeSync.js">the
+custom argumentsStringifier test</a> for another example.
+
+
+Installation
+------------
+
+Make sure you have node.js and npm installed, then run:
+
+    npm install memoizesync
+
+License
+-------
+
+3-clause BSD license -- see the `LICENSE` file for details.
---
+++ test/memoizeSync.js
@@ -0,0 +1,79 @@
+var memoizeSync = require('../lib/memoizeSync'),
+    expect = require('expect.js');
+
+describe('memoizeSync', function () {
+    it('on a zero-param function should keep returning the same result', function () {
+        var nextNumber = 1,
+            memoizedGetNextNumber = memoizeSync(function getNextNumber() {
+                return nextNumber++;
+            });
+
+        expect(memoizedGetNextNumber()).to.equal(1);
+        expect(memoizedGetNextNumber()).to.equal(1);
+    });
+
+    it('on a multi-param function should only return the same result when the parameters are the same', function () {
+        var nextNumber = 1,
+            memoizedSumOfOperandsPlusNextNumber = memoizeSync(function sumOfOperandsPlusNextNumber(op1, op2) {
+                return op1 + op2 + nextNumber++;
+            });
+
+        expect(memoizedSumOfOperandsPlusNextNumber(10, 10)).to.equal(21);
+        expect(memoizedSumOfOperandsPlusNextNumber(10, 10)).to.equal(21);
+        expect(memoizedSumOfOperandsPlusNextNumber(10, 20)).to.equal(32);
+        memoizedSumOfOperandsPlusNextNumber.purge(10, 20);
+        expect(memoizedSumOfOperandsPlusNextNumber(10, 20)).to.equal(33);
+        expect(memoizedSumOfOperandsPlusNextNumber(10, 10)).to.equal(21);
+        memoizedSumOfOperandsPlusNextNumber.purgeAll();
+        expect(memoizedSumOfOperandsPlusNextNumber(10, 20)).to.equal(34);
+        expect(memoizedSumOfOperandsPlusNextNumber(10, 10)).to.equal(25);
+    });
+
+    it('should produce a function that works as a method', function () {
+        function Counter() {
+            this.nextNumber = 1;
+        }
+
+        Counter.prototype.getNextNumber = memoizeSync(function () {
+            return this.nextNumber++;
+        });
+
+        var counter = new Counter();
+
+        expect(counter.getNextNumber()).to.equal(1);
+        expect(counter.nextNumber).to.equal(2);
+        expect(counter.getNextNumber()).to.equal(1);
+        counter.getNextNumber.purge();
+        expect(counter.getNextNumber()).to.equal(2);
+        expect(counter.nextNumber).to.equal(3);
+    });
+
+    it('should work with a custom argumentsStringifier', function () {
+        function toCanonicalJson(obj) {
+            return JSON.stringify(function traverseAndSortKeys(obj) {
+                if (Array.isArray(obj)) {
+                    return obj.map(traverseAndSortKeys);
+                } else if (typeof obj === 'object' && obj !== null) {
+                    var resultObj = {};
+                    Object.keys(obj).sort().forEach(function (key) {
+                        resultObj[key] = traverseAndSortKeys(obj[key]);
+                    });
+                    return resultObj;
+                } else {
+                    return obj;
+                }
+            }(obj));
+        }
+
+        var nextNumber = 1,
+            memoizedGetNextNumber = memoizeSync(function getNextNumber(obj, cb) {
+                return nextNumber++;
+            }, function (args) {
+                return args.map(toCanonicalJson).join('\x1d');
+            });
+
+        expect(memoizedGetNextNumber({foo: 'bar', quux: 'baz'})).to.equal(1);
+        expect(memoizedGetNextNumber({quux: 'baz', foo: 'bar'})).to.equal(1);
+        expect(memoizedGetNextNumber({barf: 'baz'})).to.equal(2);
+    });
+});
