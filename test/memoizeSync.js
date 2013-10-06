c67427fccaf8d0ca012dddb000716f8db7dda59c
Andreas Lind Petersen <andreas@one.com>
2013-10-06 18:13:31 +0200

    Replace expect.js with unexpected, update mocha to 1.13.0.

--- test/memoizeSync.js
+++ test/memoizeSync.js
@@ -1,5 +1,5 @@
 var memoizeSync = require('../lib/memoizeSync'),
-    expect = require('expect.js');
+    expect = require('unexpected');
 
 describe('memoizeSync', function () {
     it('on a zero-param function should keep returning the same result', function () {
@@ -8,8 +8,8 @@ describe('memoizeSync', function () {
                 return nextNumber++;
             });
 
-        expect(memoizedGetNextNumber()).to.equal(1);
-        expect(memoizedGetNextNumber()).to.equal(1);
+        expect(memoizedGetNextNumber(), 'to equal', 1);
+        expect(memoizedGetNextNumber(), 'to equal', 1);
     });
 
     it('on a multi-param function should only return the same result when the parameters are the same', function () {
@@ -18,15 +18,15 @@ describe('memoizeSync', function () {
                 return op1 + op2 + nextNumber++;
             });
 
-        expect(memoizedSumOfOperandsPlusNextNumber(10, 10)).to.equal(21);
-        expect(memoizedSumOfOperandsPlusNextNumber(10, 10)).to.equal(21);
-        expect(memoizedSumOfOperandsPlusNextNumber(10, 20)).to.equal(32);
+        expect(memoizedSumOfOperandsPlusNextNumber(10, 10), 'to equal', 21);
+        expect(memoizedSumOfOperandsPlusNextNumber(10, 10), 'to equal', 21);
+        expect(memoizedSumOfOperandsPlusNextNumber(10, 20), 'to equal', 32);
         memoizedSumOfOperandsPlusNextNumber.purge(10, 20);
-        expect(memoizedSumOfOperandsPlusNextNumber(10, 20)).to.equal(33);
-        expect(memoizedSumOfOperandsPlusNextNumber(10, 10)).to.equal(21);
+        expect(memoizedSumOfOperandsPlusNextNumber(10, 20), 'to equal', 33);
+        expect(memoizedSumOfOperandsPlusNextNumber(10, 10), 'to equal', 21);
         memoizedSumOfOperandsPlusNextNumber.purgeAll();
-        expect(memoizedSumOfOperandsPlusNextNumber(10, 20)).to.equal(34);
-        expect(memoizedSumOfOperandsPlusNextNumber(10, 10)).to.equal(25);
+        expect(memoizedSumOfOperandsPlusNextNumber(10, 20), 'to equal', 34);
+        expect(memoizedSumOfOperandsPlusNextNumber(10, 10), 'to equal', 25);
     });
 
     it('should produce a function that works as a method', function () {
@@ -40,12 +40,12 @@ describe('memoizeSync', function () {
 
         var counter = new Counter();
 
-        expect(counter.getNextNumber()).to.equal(1);
-        expect(counter.nextNumber).to.equal(2);
-        expect(counter.getNextNumber()).to.equal(1);
+        expect(counter.getNextNumber(), 'to equal', 1);
+        expect(counter.nextNumber, 'to equal', 2);
+        expect(counter.getNextNumber(), 'to equal', 1);
         counter.getNextNumber.purge();
-        expect(counter.getNextNumber()).to.equal(2);
-        expect(counter.nextNumber).to.equal(3);
+        expect(counter.getNextNumber(), 'to equal', 2);
+        expect(counter.nextNumber, 'to equal', 3);
     });
 
     it('should work with a custom argumentsStringifier', function () {
@@ -74,9 +74,9 @@ describe('memoizeSync', function () {
                 }
             });
 
-        expect(memoizedGetNextNumber({foo: 'bar', quux: 'baz'})).to.equal(1);
-        expect(memoizedGetNextNumber({quux: 'baz', foo: 'bar'})).to.equal(1);
-        expect(memoizedGetNextNumber({barf: 'baz'})).to.equal(2);
+        expect(memoizedGetNextNumber({foo: 'bar', quux: 'baz'}), 'to equal', 1);
+        expect(memoizedGetNextNumber({quux: 'baz', foo: 'bar'}), 'to equal', 1);
+        expect(memoizedGetNextNumber({barf: 'baz'}), 'to equal', 2);
     });
 
     it('with a maxAge should recompute the value after it has become stale', function (done) {
@@ -85,10 +85,10 @@ describe('memoizeSync', function () {
                 return nextNumber++;
             }, {maxAge: 10});
 
-        expect(memoizedGetNextNumber()).to.equal(1);
-        expect(memoizedGetNextNumber()).to.equal(1);
+        expect(memoizedGetNextNumber(), 'to equal', 1);
+        expect(memoizedGetNextNumber(), 'to equal', 1);
         setTimeout(function () {
-            expect(memoizedGetNextNumber()).to.equal(2);
+            expect(memoizedGetNextNumber(), 'to equal', 2);
             done();
         }, 15);
     });
@@ -99,13 +99,13 @@ describe('memoizeSync', function () {
                 return otherNumber + (nextNumber++);
             }, {max: 2});
 
-        expect(memoizedGetNextNumberPlusOtherNumber(1)).to.equal(2);
-        expect(memoizedGetNextNumberPlusOtherNumber.peek(1)).to.equal(2);
-        expect(memoizedGetNextNumberPlusOtherNumber(2)).to.equal(4);
-        expect(memoizedGetNextNumberPlusOtherNumber(1)).to.equal(2);
+        expect(memoizedGetNextNumberPlusOtherNumber(1), 'to equal', 2);
+        expect(memoizedGetNextNumberPlusOtherNumber.peek(1), 'to equal', 2);
+        expect(memoizedGetNextNumberPlusOtherNumber(2), 'to equal', 4);
+        expect(memoizedGetNextNumberPlusOtherNumber(1), 'to equal', 2);
         // This will purge memoizedGetNextNumberPlusOtherNumber(2):
-        expect(memoizedGetNextNumberPlusOtherNumber(3)).to.equal(6);
-        expect(memoizedGetNextNumberPlusOtherNumber(2)).to.equal(6);
+        expect(memoizedGetNextNumberPlusOtherNumber(3), 'to equal', 6);
+        expect(memoizedGetNextNumberPlusOtherNumber(2), 'to equal', 6);
     });
 
     it('with a length function should count correctly', function () {
@@ -126,12 +126,12 @@ describe('memoizeSync', function () {
                 }
             });
         memoizedFunctionThatReturnsALongStringEverySecondTime(1);
-        expect(memoizedFunctionThatReturnsALongStringEverySecondTime.cache.length).to.equal(1);
+        expect(memoizedFunctionThatReturnsALongStringEverySecondTime.cache.length, 'to equal', 1);
         memoizedFunctionThatReturnsALongStringEverySecondTime(2);
-        expect(memoizedFunctionThatReturnsALongStringEverySecondTime.cache.length).to.equal(11);
+        expect(memoizedFunctionThatReturnsALongStringEverySecondTime.cache.length, 'to equal', 11);
         memoizedFunctionThatReturnsALongStringEverySecondTime(1);
-        expect(memoizedFunctionThatReturnsALongStringEverySecondTime.cache.length).to.equal(11);
+        expect(memoizedFunctionThatReturnsALongStringEverySecondTime.cache.length, 'to equal', 11);
         memoizedFunctionThatReturnsALongStringEverySecondTime(3);
-        expect(memoizedFunctionThatReturnsALongStringEverySecondTime.cache.length).to.equal(12);
+        expect(memoizedFunctionThatReturnsALongStringEverySecondTime.cache.length, 'to equal', 12);
     });
 });
